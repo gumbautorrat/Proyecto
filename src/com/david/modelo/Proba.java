ea186f9d0ab90adb0daf50301481410937b35b71
DAVID <gumbautorrat@hotmail.com>
2014-08-04 23:51:03 +0200

    Segona modificació

--- src/com/david/modelo/Proba.java
+++ src/com/david/modelo/Proba.java
@@ -4,7 +4,8 @@ public class Proba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HOLA");
+		int a = 0;
+		System.out.println("HOLA RAMONA");
 	}
 
 }
