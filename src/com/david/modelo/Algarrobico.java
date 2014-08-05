2ca8bf5debba45dfb5891d691712120f5a88bf74
DAVID <gumbautorrat@hotmail.com>
2014-08-05 07:54:01 +0200

    Añadido 2 algarrobico

--- src/com/david/modelo/Algarrobico.java
+++ src/com/david/modelo/Algarrobico.java
@@ -5,6 +5,7 @@ public class Algarrobico {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("La puta madre que los parió");
+		System.out.println("La puta madre que los parió");
 	}
 
 }
