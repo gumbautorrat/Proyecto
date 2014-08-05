52d6408be9ea84164de539717a5006e9f4cc1e1e
DAVID <gumbautorrat@hotmail.com>
2014-08-05 07:32:18 +0200

    S'acabo

--- src/com/david/modelo/Proba.java
+++ src/com/david/modelo/Proba.java
@@ -3,10 +3,7 @@ package com.david.modelo;
 public class Proba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = 0;
-		System.out.println("MACARRONS");
-		System.out.println("PETRA");
+		System.out.println("SACABO");
 	}
 
 }
